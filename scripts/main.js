ec72dfee670ca87e1294482d65b0871e0a66a901
joaoabjr <junioralmeida_15@hotmail.com>
2022-07-28 13:58:07 -0300

    v 1.1

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,25 @@
+function validate(){
+    let validation = document.querySelector('#label-user').value;
+    console.log(validation)
+    if (validation == "") 
+      return false;
+  }
+
 function setCheckedState(){
     let state = document.getElementById('checkbox-remember-user').checked;
     document.getElementById('checkbox-remember-user').checked = state
 }
+
+function listener() { 
+    let user = document.getElementById('user')
+    let password = document.getElementById('password')
+
+    let listaUser = JSON.parse(localStorage.getItem('user'))
+
+    localStorage.setItem('user', JSON.stringify(listaUser))
+   
+    console.log(user.value)
+    console.log(password.value)
+}
+
+
